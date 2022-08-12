3ec1e8207d21f00332e206261254575f4d2ed872
ViniciusLimaFernandes <viniciuslima.4k@gmail.com>
2022-08-12 01:37:11 -0300

    Home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import logo from './assets/logo.png'
-import bgvideo from './assets/bg-vid.mp4'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import { Navbar } from 'react-bootstrap';
@@ -7,12 +6,19 @@ import { Navbar } from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
+      <link href="//db.onlinewebfonts.com/c/ed6103f87d67647f670014e8bb40678b?family=Inter+ExtraBold" rel="stylesheet" type="text/css"/>
+      <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'/>
       <Navbar bg='navbar-config' variant='navbar-config'>
         <Navbar.Brand className='navbar-brand'>
           <img src={logo} className='navbar-logo' alt='logo' />
         </Navbar.Brand>
       </Navbar>
-      <video src={bgvideo} type='video/mp4' autoplay='true' muted loop className='bg-video' />
+      <div className='Home-content'>
+        <h1 id='home-title'>Blockchain,</h1>
+        <h1 id='home-title'>IoT,</h1>
+        <h1 id='home-title'>Inovação</h1>
+        <p id='home-subtitle'>Mantenha o controle dos seus dispositivos de forma segura e transparente. </p>
+      </div>
     </div>
   );
 }
